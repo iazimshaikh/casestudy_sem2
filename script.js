e93c776525ae96a4fa6bc563d0fa21d0d8c8ffe9
Azim Shaikh <150329974+iazimshaikh@users.noreply.github.com>
2024-10-15 21:34:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function ys()
+{
+    window.open("https:\\youtube.com","","width=100% height=100%");
+}
+function becamehackerred()
+{
+    location.href='becamehacker.html';
+}
+function cryptored()
+{
+    location.href='cryptography.html';
+}
+function comnetred()
+{
+    location.href='computernetwork.html';
+}
+function hackstepred()
+{
+    location.href='hackingsteps.html';
+}
+function linuxcommandsred()
+{
+    location.href='linuxcommands.html'
+}
+function securebrowserred()
+{
+    location.href='securebrowser.html'
+}
